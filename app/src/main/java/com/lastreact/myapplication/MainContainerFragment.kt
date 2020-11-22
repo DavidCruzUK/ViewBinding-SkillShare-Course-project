5e6ac5cb273baa773df21f3d6932861d0d4dcdc0
David Cruz Anaya <me@davidcruz.co.uk>
2020-11-22 19:15:00 +0000

    03 view binding in a fragment already inflated - END

---
+++ app/src/main/java/com/lastreact/myapplication/MainContainerFragment.kt
@@ -0,0 +1,24 @@
+package com.lastreact.myapplication
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.lastreact.myapplication.databinding.FragmentMainContainerBinding
+
+class MainContainerFragment: Fragment(R.layout.fragment_main_container) {
+
+    private var _binding: FragmentMainContainerBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentMainContainerBinding.bind(view)
+        binding.textViewContainer.text = "Hi Fragment Container!"
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+}
