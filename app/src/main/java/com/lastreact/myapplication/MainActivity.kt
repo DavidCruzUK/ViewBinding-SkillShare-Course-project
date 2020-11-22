30d5ea986d80f0ed42964c9847366d45aad71a3d
David Cruz Anaya <me@davidcruz.co.uk>
2020-11-22 18:37:23 +0000

    01 - END

---
+++ app/src/main/java/com/lastreact/myapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.lastreact.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lastreact.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.textViewDemo.text = "Hello Text view Demo!"
+        setContentView(binding.root)
+    }
+}
